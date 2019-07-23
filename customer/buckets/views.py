059a3a923c4ecc2da78f0f2f6d082780c6243a38
Spielerr007 <kkgstud007@gmail.com>
2019-07-23 08:51:06 +0530

    updated with new input csv

--- customer/buckets/views.py
+++ customer/buckets/views.py
@@ -1,33 +1,25 @@
 from django.shortcuts import render
 import pandas as pd
 
+df=pd.read_csv("file:///home/spielerr/Spielerr/MIL%2019/code/Customer-Feedback-Analysis/customer/buckets/New_Data.csv")
 def fun(search_topic):
     #print(search_topic)
-    f=pd.read_csv("C:\\Users\\dell\\python\\Final_Hotles_data\\All_Data.csv")
+    
+    df_temp=df[df['Hotel Name']==search_topic]
 
-    ambience=f[f['Bucket']=='ambience']
-    ambience=ambience[ambience['Hotel Name']==search_topic]['Review'].head().tolist()
-
-    food=f[f['Bucket']=='food']
-    food=food[food['Hotel Name']==search_topic]['Review'].head().tolist()
-
-    price=f[f['Bucket']=='price']
-    price=price[price['Hotel Name']==search_topic]['Review'].head().tolist()
-     
-    amenities=f[f['Bucket']=='amenities']
-    amenities=amenities[amenities['Hotel Name']==search_topic]['Review'].head().tolist()
-     
-    facility=f[f['Bucket']=='facility']
-    facility=facility[facility['Hotel Name']==search_topic]['Review'].head().tolist()
-     
-    cleanliness=f[f['Bucket']=='cleanliness']
-    cleanliness=cleanliness[cleanliness['Hotel Name']==search_topic]['Review'].head().tolist()
-     
-    staff=f[f['Bucket']=='staff']
-    staff=staff[staff['Hotel Name']==search_topic]['Review'].head().tolist()
-
-    buckets={'name':search_topic,'ambience':ambience,'food':food,'staff':staff,'amenities':amenities,'cleanliness':cleanliness,'facility':facility,'price':price}
-    #print(buckets)
+    buckets={'ambience':[],'amenities':[],'cleanliness':[],'facility':[],'food':[],'price':[],'staff':[], 'name':search_topic, 'image': 'img/analysis/' + search_topic + '.jpg'}
+    for bucket in buckets:
+    #print(bucket)
+        df_bucket=df_temp[df_temp['Bucket']==bucket]
+    #print(len(df_bucket))
+        ctr=1
+        for index,row in df_bucket.iterrows():
+            if len(buckets[bucket])>4:
+                break
+            buckets[bucket].append((ctr,row['Review'],row['Recency']))
+            ctr+=1
+    
+    #buckets={'name':search_topic,'ambience':[ambience,amb_date], 'food':[food,food_date], 'staff':[staff,staff_date], 'amenities':[amenities,amen_date], 'cleanliness':[cleanliness,clean_date], 'facility':[facility,fac_date], 'price':[price, price_date]}
     return buckets
 
 
