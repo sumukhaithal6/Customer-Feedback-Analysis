5161e9e2d466ce9f50adc06a34a67155c255f105
Spielerr007 <kkgstud007@gmail.com>
2019-08-27 14:36:00 +0530

    added search bar

---
+++ customer/interface/static/js/hotels.js
@@ -0,0 +1,15 @@
+// JavaScript code 
+function search_hotel() { 
+    let input = document.getElementById('searchbar').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementsByClassName('portfolio-item'); 
+      
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="";                  
+        } 
+    } 
+} 
